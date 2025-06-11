880da7955e3433ef3f6cbf53722425355fd0af76
asif0808 <aasif911496@gmail.com>
2025-06-11 11:45:58 +0530

    Starting

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Hello from CreateReactApp</h1>
+  );
+}
+
+export default App;
---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+
+
+  return (
+    <h1>Hello from vite</h1>
+  )
+}
+
+export default App
